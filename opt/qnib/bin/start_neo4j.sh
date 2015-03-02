6fbcee19bbcd5d6e95e335587eac3bbc7ea22f44
Christian Kniep <christian@qnib.org>
2015-03-02 15:43:37 +0100

    better way of checking if neo4j is running

--- opt/qnib/bin/start_neo4j.sh
+++ opt/qnib/bin/start_neo4j.sh
@@ -9,17 +9,35 @@ cd $NEO4J_HOME
 
 
 function watch_pid {
+    sleep 2
     while [ true ];do
         PID=$(cat ${PID_FILE})
-        if [ kill ${PID} ]; then
+        if [ -d "/proc/${PID}" ]; then
             sleep 1
+	else 
+            return 0
         fi
     done
+}
+
+function wait_running {
+    curl -s -XGET http://localhost:7474/db/data/ 1>/dev/null 2>&1
+    if [ $? -ne 0 ];then
+        sleep 1
+        wait_running
+    fi
 }
 
 function enable_index {
     curl -XPUT http://localhost:7474/db/data/index/auto/node/status -d true
     curl -XPOST http://localhost:7474/db/data/index/auto/node/properties -d name
+    curl -XPOST http://localhost:7474/db/data/index/node/ -d """{
+        "name" : "node_auto_index",
+        "config" : {
+            "type" : "fulltext",
+            "provider" : "lucene"
+        }
+    }"""
 }
 
 function stop () {
@@ -37,5 +55,8 @@ java -cp "${CLASSPATH}" \
         -Dorg.neo4j.server.properties=conf/neo4j-server.properties \
         org.neo4j.server.Bootstrapper &
 echo $! > "${PID_FILE}"
+
+wait_running
+enable_index
 
 watch_pid
